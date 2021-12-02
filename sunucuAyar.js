92c4e41f97a6e6529c67ff5577f38037e672787a
DarknessCodeShare <91627572+DarknessCodeShare@users.noreply.github.com>
2021-12-02 19:17:08 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '906908701552701521', // sunucu idsi
+  ownerRole:"910956654856658995", // sunucu sahiplerinin rolü
+  teyitsizRolleri:["915994505663103049"], // kayıt olmayanlara verieln rol
+  jailRolu:["915994649821323315","",], // cezalılara verilen rol
+  enAltYetkiliRolu:["915995255860506674"], // ilk yetkili permi
+  activity:"Darkness", // botun durumu
+  status:"idle",
+  invitelink:"https://discord.gg/tgAEjpts", //davet linki
+  symbol:"✰", // tag
+  voicechannel:"906908704115408919" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
